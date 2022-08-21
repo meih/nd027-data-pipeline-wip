0365618f97a8cdc5fbac4d6ad67bec6e51e2e446
meih <meiatwork@gmail.com>
2022-08-22 00:10:38 +0900

    Update data_quality.py

--- home/airflow/plugins/operators/data_quality.py
+++ home/airflow/plugins/operators/data_quality.py
@@ -8,26 +8,31 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 tables = [],
+                 checks = [],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.tables = tables
+        self.checks = checks
 
     def execute(self, context):
         redshift_hook = PostgresHook("redshift")
 
-        for table in self.tables:
-            self.log.info(f"Data quality check start with {table}.") 
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+        for check in self.checks:
+            self.log.info(f"Data quality check started.") 
+            test_sql = check['test_sql']
+            expected_result = check['check.expected_result']
+            comparison = check['comparison']
+            result = redshift_hook.get_first_record(test_sql)
 
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed: {table} returned no results")
-
-            num_records = records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed: {table} contained 0 rows")
-
-            self.log.info(f"Data quality check passed: {table} has {records[0][0]} records")
-
-        
+            if comparison == '=':
+                if result != expected_result:
+                    raise AssertionError(f"Check failed: {result} {comparison} {expected_result}")
+            if comparison == '!=':
+                if result == expected_result:
+                    raise AssertionError(f"Check failed: {result} {comparison} {expected_result}")
+            if comparison == '>':
+                if result <= expected_result:
+                    raise AssertionError(f"Check failed: {result} {comparison} {expected_result}")
+            if comparison == '<':
+                if result <= expected_result:
+                    raise AssertionError(f"Check failed: {result} {comparison} {expected_result}")
